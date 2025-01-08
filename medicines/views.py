9f5b5283b3258447e303b6f5a67432948350da52
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-01-08 18:13:46 -0300

    build: Adiciona o djangorestframework

--- medicines/views.py
+++ medicines/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.views import View
+from .models import Medicine
+import json
 
-# Create your views here.
+class MedicineView(View):
+    def get(self, request):
+        data = list(Medicine.objects.values())
+        return HttpResponse(json.dumps(data), content_type = "application/json")

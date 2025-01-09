51f0c4b0d7fe582d1a35d01b454718f4c2a46721
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-01-09 12:48:42 -0300

    feat: Adiciona filtros em Medicine View

--- medicines/views.py
+++ medicines/views.py
@@ -1,18 +1,18 @@
-from rest_framework.pagination import PageNumberPagination
 from .models import Medicine
 from .serializer import MedicineSerializer
+from .pagination import MedicinePagination
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-class MedicinePagination(PageNumberPagination):
-    page_size = 2
-    page_size_query_param = 'page_size'
-
-class MedicineAPIView(APIView):
+class MedicineAPI(APIView):
     def get(self, request):
-        medicine = Medicine.objects.all()
+        name = request.query_params.get('name')
+        if name:
+            medicine = Medicine.objects.filter(name__icontains=name)
+        else:
+            medicine = Medicine.objects.all()
         paginator = MedicinePagination()
         page = paginator.paginate_queryset(medicine, request)
         medicine_serializer = MedicineSerializer(page, many=True)
@@ -27,7 +27,7 @@ class MedicineAPIView(APIView):
         else:
             return Response(medicineSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-class MedicineAPIViewDetail(APIView):
+class MedicineAPIDetail(APIView):
     def get(self, request, id):
         medicine = get_object_or_404(Medicine ,pk=id)
         medicineSerializer = MedicineSerializer(medicine)
@@ -38,9 +38,10 @@ class MedicineAPIViewDetail(APIView):
         medicineSerializer = MedicineSerializer(medicine, data=request.data)
         if medicineSerializer.is_valid():
             medicineSerializer.save()
-            return Response(medicineSerializer.data)
+            return Response(medicineSerializer.validated_data)
         else:
             return Response(medicineSerializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
     def delete(self, request, id):
         medicine = get_object_or_404(Medicine, pk=id)
         medicine.delete()

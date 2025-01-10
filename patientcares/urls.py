87ad852bbc2e4b75b6d20b15fd5bb935c465d51d
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-01-10 10:57:35 -0300

    feat: Adiciona o CRUD de atendimentos

--- patientcares/urls.py
+++ patientcares/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.PatientCareView.as_view()),
+    path('<int:id>/', views.PatientCareViewDetail.as_view())
 ]
--- patientcares/views.py
+++ patientcares/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .models import PatientCare
+from .paginations import PatientCarePagination
+from .serializers import PatientCareSerializer
 
-# Create your views here.
+class PatientCareView(APIView):
+    def get(self, request):
+        name = request.query_params.get('name')
+        if name:
+            patientcares = PatientCare.objects.filter(name_icontains=name)
+        else:
+            patientcares = PatientCare.objects.all()
+        pagination = PatientCarePagination
+        page = pagination.paginate_queryset(patientcares, request)
+        patientcaresserializer = PatientCareSerializer(page, many=True)
+        return Response(patientcaresserializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        patientcaresserializer = PatientCareSerializer(request.data)
+        if patientcaresserializer.is_valid():
+            patientcaresserializer.save()
+            return Response(patientcaresserializer.validated_data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+class PatientCareViewDetail(APIView):
+    def get(self, request, id):
+        patientcare = get_object_or_404(PatientCare, pk=id)
+        patientcareserializer = PatientCareSerializer(patientcare)
+        return Response(patientcareserializer.data, status=status.HTTP_202_ACCEPTED)
+    
+    def put(self, request, id):
+        patientcare = get_object_or_404(PatientCare, pk=id)
+        patientcareserializer = PatientCareSerializer(patientcare , request.data)
+        if patientcareserializer.is_valid():
+            patientcareserializer.save()
+            return Response(patientcareserializer.data, status=status.HTTP_200_OK)  
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)  
+    
+    def delete(self, request, id):
+        patientcare = get_object_or_404(PatientCare, pk=id)
+        patientcare.delete()
+        return Response({"message": "Atendimento deletado com sucesso."}, status=status.HTTP_204_NO_CONTENT)

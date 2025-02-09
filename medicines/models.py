761e20269c0050eb2c55bd6baaab1ca22aa2efd6
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-02-09 20:11:06 -0300

    fix: Edita model de medicamentos para aceitar image como vazio

--- medicines/models.py
+++ medicines/models.py
@@ -5,7 +5,7 @@ class Medicine(models.Model):
     dosage = models.CharField(max_length=10)
     manufacturer = models.CharField(max_length=40,null=True)
     composition = models.CharField(max_length=40,null=True)
-    medicine_image = models.ImageField(upload_to='medicines/')
+    medicine_image = models.ImageField(upload_to='medicines/', blank=True)
 
     def __str__(self):
         return self.name + ' - ' + self.dosage

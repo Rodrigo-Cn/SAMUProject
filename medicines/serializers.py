631d5521262fa1c31714d8a75f723961639b17ff
Rodrigo-Cn <rodrigo321.costa@gmail.com>
2025-03-06 13:27:24 -0300

    feat: Adiciona base64 na imagem de medicamento

--- medicines/serializers.py
+++ medicines/serializers.py
@@ -1,7 +1,16 @@
+import base64
 from rest_framework import serializers
 from .models import Medicine
 
 class MedicineSerializer(serializers.ModelSerializer):
+    medicine_image = serializers.SerializerMethodField()
+
+    def get_medicine_image(self, obj):
+        if obj.medicine_image:
+            with open(obj.medicine_image.path, "rb") as img_file:
+                return base64.b64encode(img_file.read()).decode("utf-8")
+        return None
+
     class Meta:
         model = Medicine
-        fields = '__all__'
+        fields = ["id", "name", "dosage", "manufacturer", "medicine_image"]
